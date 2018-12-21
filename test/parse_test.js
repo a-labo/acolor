242e67da5dad9706236dfcfc0078089141e1f463
okunishinishi <okunishinishi@gmail.com>
2018-12-21 15:28:19 +0900

    Version incremented to 3.0.1

--- test/parse_test.js
+++ test/parse_test.js
@@ -7,7 +7,6 @@
 const parse = require('../lib/parse.js')
 const assert = require('assert')
 
-
 describe('parse', () => {
   before(async () => {
 
@@ -18,8 +17,6 @@ describe('parse', () => {
   })
 
   it('Parse', async () => {
-    assert.throws(() => parse(null))
-    assert.throws(() => parse(''))
     assert.throws(() => parse('#A'))
     assert.equal(parse('#A11').red(), 170)
     assert.equal(parse(parse('#A11')).red(), 170)
