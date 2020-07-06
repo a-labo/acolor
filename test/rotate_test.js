948dda959fd5e490016241affc5cd286562a49cb
FujiHaruka <fizzyjohn1427@gmail.com>
2020-07-06 09:43:10 +0900

    Add option to rotate in LCH color space

--- test/rotate_test.js
+++ test/rotate_test.js
@@ -21,6 +21,13 @@ describe('rotate', () => {
     assert.equal(rotate('#AA1', 30), 'hsl(90, 81.8%, 36.7%)')
     assert.equal(rotate('#AA1', 120), 'hsl(180, 81.8%, 36.7%)')
   })
+
+  it('Rotate in LCH color space', async () => {
+    assert.equal(rotate('rgb(170, 170, 17)', 0, { lch: true }), 'rgb(170, 170, 17)')
+    assert.equal(rotate('rgb(170, 170, 17)', 360, { lch: true }), 'rgb(170, 170, 17)')
+    assert.equal(rotate('rgb(170, 170, 17)', -360, { lch: true }), 'rgb(170, 170, 17)')
+    assert.equal(rotate('rgb(170, 170, 17)', 20, { lch: true }), 'rgb(126, 180, 48)')
+  })
 })
 
 /* global describe, before, after, it */
